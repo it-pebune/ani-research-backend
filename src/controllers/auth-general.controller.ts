3df376f0eace54e81805741fd2e0f24703f2a4d2
Razvan Stefanoiu <razvan.stefanoiu@vxmt.com>
2022-01-31 17:03:29 +0200

    fix: refresh token expire period set to 0 when refreshing auth token as valability of the refresh token is not extented

--- src/controllers/auth-general.controller.ts
+++ src/controllers/auth-general.controller.ts
@@ -125,7 +125,7 @@ export class GeneralAuthController {
         access: jwtBearerToken,
         refresh: req.body.token,
         accessExpiresIn: 2 * 60 * 60,
-        refreshExpiresIn: 24 * 60 * 60
+        refreshExpiresIn: 0
       };
 
       res.status(StatusCodes.OK).json(token);
