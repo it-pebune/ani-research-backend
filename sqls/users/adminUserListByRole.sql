304d3b8ea9cf1fb08c1b1bfdfb80f9392f267e1e
Razvan Stefanoiu <razvan.stefanoiu@vxmt.com>
2022-03-11 22:12:25 +0200

    add: subject count to get user by role

--- sqls/users/adminUserListByRole.sql
+++ sqls/users/adminUserListByRole.sql
@@ -11,10 +11,20 @@ AS
 BEGIN
   SET NOCOUNT ON
 
-  SELECT U.id, R.roleId, U.displayName, U.email
+  ;WITH UserSubjectsView
+  AS
+  (
+      SELECT userId, COUNT(1) subjectCount
+      FROM  UserSubjects
+      WHERE revoked = 0
+      GROUP BY userId
+  )
+  SELECT U.id, R.roleId, U.displayName, U.email, US.subjectCount
   FROM  [User] U
         INNER JOIN
         UserRoles R ON R.userId = U.id AND R.roleId = @roleId
+        LEFT JOIN
+        UserSubjectsView US ON U.id = US.userId
   WHERE U.deleted = 0
   ORDER BY displayName
 END
